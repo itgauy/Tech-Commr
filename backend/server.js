a3f2f201d13db9c9a76d185ddcbbc98cc3a9f51d
itgauy <torreslian@duck.com>
2024-09-09 18:37:30 +0800

    updated again with local

--- backend/server.js
+++ backend/server.js
@@ -33,24 +33,13 @@ app.use('/api', feedbacksRoutes)
 // })
 
 // db
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     // listen for requests (3000 local)
-//     app.listen(process.env.PORT, () => {
-//       console.log('connected on db and listening on port', process.env.PORT)
-//     })
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
-
-// new db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to db')
+    // listen for requests (3000 local)
+    app.listen(process.env.PORT, () => {
+      console.log('connected on db and listening on port', process.env.PORT)
+    })
   })
   .catch((error) => {
     console.log(error)
   })
-
-module.exports = app
